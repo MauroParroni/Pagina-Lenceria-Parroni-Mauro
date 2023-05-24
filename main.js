583326cf616a3b504a718f391d0bf5683aad96e3
Mauro Parroni <mauro_rp_11@live.com.ar>
2023-05-24 01:02:41 -0300

    fix Login

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ class admin {
     }
   }
     const admi = new admin("admin", "admin");
-    const verificaUsuario = document.querySelector("#card");
+    const verificaUsuario = document.querySelector("#card");//agarro el id card del form
     if (verificaUsuario) {
-      verificaUsuario.addEventListener("submit", (e) => {
+      verificaUsuario.addEventListener("submit", (e) => { //cuando mando el formulario guardo los valores en una variable
         e.preventDefault();
         const user = e.target.querySelector("#usuario").value;
         const pass = e.target.querySelector("#contra").value;
     
-        if (user === admi.usua && pass === admi.contraseña) {      
+        if (user === admi.usua && pass === admi.contraseña) {      //verifico
           window.location.href = "../index.html";
           localStorage.setItem("isLoggedIn", "true");
         }
       });
     } 
-    if (localStorage.getItem("isLoggedIn") === "true") {
+    if (localStorage.getItem("isLoggedIn") === "true") {// y aca hago desaparecer el boton de login y hago aparecer el panel de admin
         let boton = document.getElementById("booot");
         boton.style.display = "none";
         let header = document.getElementById("header");
