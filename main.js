a9268828daaf186475471bfa5a13e7ce65b39e04
MauroParroni <125755534+MauroParroni@users.noreply.github.com>
2023-05-31 00:12:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ class admin {
       });
     } 
     let url = "../Paginas/panel_admin.html"
-    console.log( window.location.href);
+    console.log( window.location.href,"funciono");
     if (window.location.pathname === "/index.html"){
       url = "./Paginas/panel_admin.html"
     }
